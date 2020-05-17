e47b757cc351912302838397b953a1364db70a8d
nsddksmc <40565225+nsddksmc@users.noreply.github.com>
2020-05-17 12:04:01 +0300

    covtaskng1

---
+++ story_content/user.js
@@ -0,0 +1,63 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "60bH5daqR01":
+        Script1();
+        break;
+      case "6NjLymk9CX5":
+        Script2();
+        break;
+      case "6TswsN53Eat":
+        Script3();
+        break;
+      case "6MrjO0YXE0G":
+        Script4();
+        break;
+      case "5hCQUeG2NZL":
+        Script5();
+        break;
+      case "6bXaKzCnDhK":
+        Script6();
+        break;
+      case "6ZjhLsV4Nt9":
+        Script7();
+        break;
+  }
+}
+
+function Script1()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script2()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script3()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script4()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script5()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script6()
+{
+  $("#tab-customlink").hide();
+}
+
+function Script7()
+{
+  window.print();
+}
+
